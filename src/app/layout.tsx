b9b71234b319eeaea17a341de11353358c92859e
Vignesh Lagishetti <lvigneshbunty789@gmail.com>
2025-01-23 15:13:55 +0530

    Changes made on mobile menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Navbar from "@/components/Navbar"
+import { ClerkProvider } from "@clerk/nextjs"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Lama Dev Social Media App",
+  title: "Vinsa-Media",
   description: "Social media app built with Next.js",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <ClerkProvider>
@@ -29,5 +29,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  );
+  )
 }
